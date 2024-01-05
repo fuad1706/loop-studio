3c850b54b040b3579609cf8871f3832a0ebaa826
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-05 14:15:18 -0800

    nav-mobile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let hamburger = document.querySelector(".hamburger");
 let nav = document.querySelector("nav");
 
 hamburger.onclick = function(){
-    nav.classList.toggle(".open-nav");
+    nav.classList.toggle("open-nav");
 
     if(nav.classList.contains("open-nav")){
         hamburger.src = "./images/icon-close.svg";
