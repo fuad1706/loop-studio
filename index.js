1da8ed4612965a3fd23a01bbca8af07d9b5ba4d5
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-04 22:05:25 -0800

    sec-section

---
+++ index.js
@@ -0,0 +1,13 @@
+let hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector("nav");
+
+hamburger.onclick = function(){
+    nav.classList.toggle(".open-nav");
+
+    if(nav.classList.contains(".open-nav")){
+        hamburger.src = "./images/icon-close.svg"
+    }
+    else{
+        hamburger.src = "./images/icon-hamburger.svg"
+    }
+}
