e0b21ed2065b90e5c3a1ab92cb20a6b4fdd7cf49
fuad1706 <fuadshorlarxwix@gmail.com>
2024-01-05 11:36:10 -0800

    media-foot

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ let nav = document.querySelector("nav");
 hamburger.onclick = function(){
     nav.classList.toggle(".open-nav");
 
-    if(nav.classList.contains(".open-nav")){
-        hamburger.src = "./images/icon-close.svg"
+    if(nav.classList.contains("open-nav")){
+        hamburger.src = "./images/icon-close.svg";
     }
     else{
-        hamburger.src = "./images/icon-hamburger.svg"
+        hamburger.src = "./images/icon-hamburger.svg";
     }
 }
